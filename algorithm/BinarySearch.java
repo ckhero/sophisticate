3a08ae9a859783abeb90f9a858e6abe49d4eae02
陈凯 <user@userdeMac-mini-3.local>
2019-12-09 10:46:29 +0800

    fix

--- algorithm/BinarySearch.java
+++ algorithm/BinarySearch.java
@@ -8,7 +8,7 @@ public class BinarySearch {
     public static void main(String[] args) throws Exception {
         try {
             int[] arr = {1,2,3,5};
-            int key = 5;
+            int key = 8;
             System.out.println("res = " + unRecursion(arr, key));
             System.out.println("res = " + recursion(arr, key, 0, arr.length - 1));
         } catch (Exception e) {
@@ -23,46 +23,40 @@ public class BinarySearch {
      * @return
      * @throws Exception
      */
-    private static int unRecursion(int[] arr, int key) throws Exception {
+   public static int unRecursion(int[] arr,  int key) throws Exception {
         int low = 0;
         int high = arr.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-
-            if (arr[mid] == key) {
+            int midVal = arr[mid];
+            if (midVal == key) {
                 return mid;
             }
-
-            if (arr[mid] > key) {
+            if (midVal > key) {
                 high = mid - 1;
                 continue;
             }
             low = mid + 1;
         }
-        throw new Exception("don't match anykey");
-    }
+        throw new Exception("not match any val");
+   }
 
-    /**
-     * 递归
-     * @param arr
-     * @param key
-     * @param low
-     * @param high
-     * @return
-     * @throws Exception
-     */
-    private static int recursion(int[] arr, int key, int low, int high) throws Exception {
+    public static int recursion(int[] arr,  int key, int low, int high) throws Exception {
         if (low > high) {
-            throw new Exception("don't match anykey");
+            throw new Exception("not match any val");
         }
         int mid = (low + high) / 2;
-        if (arr[mid] == key) {
+        int midVal = arr[mid];
+
+        if (midVal == key) {
             return mid;
         }
-        if (arr[mid] > key) {
-            return recursion(arr, key, low, mid - 1);
+
+        if (midVal > key) {
+            mid = recursion(arr, key , low, mid - 1);
         } else {
-            return recursion(arr, key, mid + 1, high);
+            mid = recursion(arr, key , mid + 1, high );
         }
+        return mid;
     }
-}
+    }
